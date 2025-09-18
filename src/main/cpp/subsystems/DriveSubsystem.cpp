9c069213a286fc62cf846b24ea2ef722bf919209
StarlessKnights <119544209+StarlessKnights@users.noreply.github.com>
2025-09-18 16:53:34 -0500

    Make Drivebase use the Constants (#6)

--- src/main/cpp/subsystems/DriveSubsystem.cpp
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -14,10 +14,19 @@
 #include "utils/TurboPoseEstimator.hpp"
 
 DriveSubsystem::DriveSubsystem()
-    : fleft(1, 2, 3, 0.0, DriveSubsystemConstants::kCanivoreName),
-      fright(4, 5, 6, 0.0, DriveSubsystemConstants::kCanivoreName),
-      bleft(7, 8, 9, 0.0, DriveSubsystemConstants::kCanivoreName),
-      bright(11, 12, 13, 0.0, DriveSubsystemConstants::kCanivoreName),
+    : fleft(DriveSubsystemConstants::kFrontLeftDriveID, DriveSubsystemConstants::kFrontLeftSteerID,
+            DriveSubsystemConstants::kFrontLeftEncoderID, DriveSubsystemConstants::kFrontLeftOffset,
+            DriveSubsystemConstants::kCanivoreName),
+      fright(DriveSubsystemConstants::kFrontRightDriveID,
+             DriveSubsystemConstants::kFrontRightSteerID,
+             DriveSubsystemConstants::kFrontRightEncoderID,
+             DriveSubsystemConstants::kFrontRightOffset, DriveSubsystemConstants::kCanivoreName),
+      bleft(DriveSubsystemConstants::kBackLeftDriveID, DriveSubsystemConstants::kBackLeftSteerID,
+            DriveSubsystemConstants::kBackLeftEncoderID, DriveSubsystemConstants::kBackLeftOffset,
+            DriveSubsystemConstants::kCanivoreName),
+      bright(DriveSubsystemConstants::kBackRightDriveID, DriveSubsystemConstants::kBackRightSteerID,
+             DriveSubsystemConstants::kBackRightEncoderID,
+             DriveSubsystemConstants::kBackRightOffset, DriveSubsystemConstants::kCanivoreName),
       field(),
       estimator(GetAngle(), GetModulePositions(), frc::Pose2d()) {
   frc::SmartDashboard::PutData("Field", &field);
