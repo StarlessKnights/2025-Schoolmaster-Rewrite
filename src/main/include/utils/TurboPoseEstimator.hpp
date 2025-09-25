be4166b444f81fd0c44f6ee72f76986c88d0ab4c
StarlessKnights <StarlessKnights@users.noreply.github.com>
2025-09-25 15:04:46 -0500

    feat: some integration with the pose estimator

--- src/main/include/utils/TurboPoseEstimator.hpp
+++ src/main/include/utils/TurboPoseEstimator.hpp
@@ -5,22 +5,25 @@
 #include "frc/geometry/Pose2d.h"
 #include "frc/geometry/Rotation2d.h"
 #include "frc/kinematics/SwerveModulePosition.h"
+#include "utils/TurboPhotonCamera.hpp"
+#include <array>
 
 class TurboPoseEstimator {
- private:
+private:
   frc::SwerveDrivePoseEstimator<4> poseEstimator;
 
- public:
-  TurboPoseEstimator(frc::Rotation2d gyroAngle,
-                     std::array<frc::SwerveModulePosition, 4> modulePositions,
+  std::array<class TurboPhotonCamera, 2> localizationCameras = {
+      TurboPhotonCamera(CameraConstants::kLocalizationCamOneName, CameraConstants::kLocalizationCamOneOffset),
+      TurboPhotonCamera(CameraConstants::kLocalizationCamTwoName, CameraConstants::kLocalizationCamTwoOffset)};
+
+public:
+  TurboPoseEstimator(frc::Rotation2d gyroAngle, std::array<frc::SwerveModulePosition, 4> modulePositions,
                      frc::Pose2d initialPose)
-      : poseEstimator(DriveSubsystemConstants::kKinematics, gyroAngle, modulePositions,
-                      initialPose) {}
+      : poseEstimator(DriveSubsystemConstants::kKinematics, gyroAngle, modulePositions, initialPose) {}
 
   frc::Pose2d getPose2D();
-  void ResetEstimatorPosition(frc::Rotation2d gyroAngle,
-                              std::array<frc::SwerveModulePosition, 4> modulePositions,
+  void ResetEstimatorPosition(frc::Rotation2d gyroAngle, std::array<frc::SwerveModulePosition, 4> modulePositions,
                               frc::Pose2d pose);
-  void UpdateWithOdometry(frc::Rotation2d gyroAngle,
-                          std::array<frc::SwerveModulePosition, 4> modulePositions);
+  void UpdateWithOdometry(frc::Rotation2d gyroAngle, std::array<frc::SwerveModulePosition, 4> modulePositions);
+  void TryVisionUpdateWithCamera(const TurboPhotonCamera &camera);
 };
